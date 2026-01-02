bb39899d8743cd5f32b928e64524ed2c862938ee
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-01-02 21:23:25 +0700

    Time: 0 ms (100%), Space: 17.1 MB (98.28%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        
+        # 2 3 : 5 9
+        t = list(time)
+        
+        if t[0] == "?":
+
+            if t[1] == "?" or t[1] <= "3":
+                t[0] = "2"
+            else:
+                t[0] = "1"
+        if t[1] == "?":
+
+            if t[0] == "2":
+                t[1] = "3"
+            else:
+                t[1] = "9"
+        
+        if t[3] == "?":
+            t[3] = "5"
+        
+        if t[4] == "?":
+            t[4] = "9"
+        
+        return "".join(t)
