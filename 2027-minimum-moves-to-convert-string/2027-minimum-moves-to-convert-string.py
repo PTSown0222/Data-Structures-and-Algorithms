30e37bb4adcec96301ebb6af3668e93468a1e37f
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-01-03 00:16:46 +0700

    Time: 1 ms (36.58%), Space: 17.2 MB (95.97%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        move = 0
+        i = 0
+        while i < len(s):
+            if s[i] == 'X':
+                i+=3
+                move+=1
+            else:
+                i += 1
+        return move
