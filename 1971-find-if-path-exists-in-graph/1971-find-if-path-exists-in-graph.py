7157efb0465b9754fce3b1855937e51195e2db93
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-01-04 11:05:35 +0700

    Time: 535 ms (51.96%), Space: 137.8 MB (26.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,33 +1,64 @@
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # code by BFS
-        visited = [False] * n 
-        visited[source] = True
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#         # code by BFS - clear
+#         visited = [False] * n 
+#         visited[source] = True
         
-        # build Adj List
-        adj = [[] for _ in range(n)]
+#         # build from edege-List to Adj-List
+#         adj = [[] for _ in range(n)]
+#         for i in range(len(edges)):
+#             u = edges[i][0]
+#             v = edges[i][1]
+            
+#             adj[u].append(v) 
+#             adj[v].append(u) 
 
-        # traverse
-        for i in range(len(edges)):
-            u = edges[i][0]
-            v = edges[i][1]
+#         queue = [source]
+#         while len(queue) > 0:
+#             current_node = queue.pop(0)
             
-            adj[u].append(v) 
-            adj[v].append(u) 
-
-        queue = [source]
-        while len(queue) > 0:
-            current_node = queue.pop(0)
+#             if current_node == destination:
+#                 return True
             
-            if current_node == destination:
-                return True
-            
-            for neighbor in adj[current_node]:
-                if visited[neighbor] == False:
-                    visited[neighbor] = True
-                    queue.append(neighbor)
-        return False
+#             for neighbor in adj[current_node]:
+#                 if visited[neighbor] == False:
+#                     visited[neighbor] = True
+#                     queue.append(neighbor)
+#         return False
 
         # time complexity O(V + E) with the number of vertices and E being the number of edges
         # space complexity O(V + e)
+
+#------DFS-----#
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        #graph = {0: []}
+        graph = collections.defaultdict(list)
+        for u, v in edges:
+            #graph = {0: [1]}
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        def dfs(node:int, visted: List[int]):
+            
+            if node == destination:
+                return True
+            
+            # visited -> it's used
+            visited.add(node)
+
+            #graph = {0: [1, 2, 3]}
+            # for neighbor in [1,2,3]:
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor, visited):
+                        return True
+            return False
+        
+        visited = set()
+        return dfs(source, visited)
+
+
+            
 
