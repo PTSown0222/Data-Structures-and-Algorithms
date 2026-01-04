ee406fa8e009724110186c0221042ffb8073b22f
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-01-04 11:36:58 +0700

    Time: 0 ms (100%), Space: 40.6 MB (95.56%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,29 +1,28 @@
-# class Solution:
-#     def findCenter(self, edges: List[List[int]]) -> int:
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
         
-#         edge1 = edges[0]
-#         edge2 = edges[1]
+        edge1 = edges[0]
+        edge2 = edges[1]
 
-#         if edge1[0] == edge2[0] or edge1[0] == edge2[1]:
-#             return edge1[0]
-#         else:
-#             return edge1[1]
+        if edge1[0] == edge2[0] or edge1[0] == edge2[1]:
+            return edge1[0]
+        else:
+            return edge1[1]
 
 
 #--- adj_list---#
-from collections import defaultdict
-
-class Solution:
-    def findCenter(self, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
+# from collections import defaultdict
+# class Solution:
+#     def findCenter(self, edges: List[List[int]]) -> int:
+#         graph = defaultdict(list)
         
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+#         for u, v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
 
-        total_edges = len(edges) 
-        for node in graph:
-            if len(graph[node]) == total_edges:
-                return node
+#         total_edges = len(edges) 
+#         for node in graph:
+#             if len(graph[node]) == total_edges:
+#                 return node
                 
-        return -1
+#         return -1
