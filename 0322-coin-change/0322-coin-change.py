c5e53c48c449faed169b6691a4e52aaeaf8f2005
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-01-28 17:10:09 +0700

    Time: 459 ms (90.53%), Space: 19.8 MB (32.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,17 +2,31 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # dp(amount) = Return the fewest number of coins 
 
-        @lru_cache(None)
-        def dp(amount):
-            if amount < 0:
-                return float("inf")
-            #no amount == no coin
-            if amount == 0:
-                return 0
-            ans = float("inf")
-            for coin in coins:
-                ans = min(ans, dp(amount - coin) + 1)
+        # top-down DP
+        # @lru_cache(None)
+        # def dp(amount):
+        #     if amount < 0:
+        #         return float("inf")
+        #     #no amount == no coin
+        #     if amount == 0:
+        #         return 0
+        #     ans = float("inf")
+        #     for coin in coins:
+        #         ans = min(ans, dp(amount - coin) + 1)
             
-            return ans
-        res = dp(amount)
-        return -1 if res == float("inf") else res
+        #     return ans
+        # res = dp(amount)
+        # return -1 if res == float("inf") else res
+
+        # Bottom-up
+        dp = [float('inf')] * (amount + 1)
+        
+        dp[0]=0
+        for i in range(amount +1):
+            for coin in coins:
+                if i  >= coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
+
+
