8aa528c3d623ef625a2f6baa3990093002e986c3
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-03-26 10:11:58 +0700

    Time: 3 ms (82.97%), Space: 20.6 MB (17.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,7 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = 0
-        store = []
         if len(nums) == 0: return 0
         for i in range(len(nums)):
             if nums[i] != 0:
