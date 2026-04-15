7b9200873e0bb241411eaa4a51a2b20cad4ff47c
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-15 16:10:57 +0700

    Time: 11 ms (24.83%), Space: 19.6 MB (7.76%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,26 +1,30 @@
-class Solution:
-    def minimumDifference(self, nums: List[int], k: int) -> int:
-        if k == 1: return 0
-        nums.sort()
-        n = len(nums)
-        res = float('inf')
-        # hard window - Khi đề bài cho chỉ số window kết thúc là K
-        n_windows_K = n - k + 1
-        for i in range(n_windows_K):
-            first = i
-            last = i + k - 1
-            window = nums[last] - nums[first]
-            res = min(res, window)
-        return res
-            
-# dynamic window - khi chưa biết đề bài cho chỉ số window cuối là bao nhiêu chỉ cho target duy nhất
 # class Solution:
 #     def minimumDifference(self, nums: List[int], k: int) -> int:
 #         if k == 1: return 0
 #         nums.sort()
 #         n = len(nums)
 #         res = float('inf')
-#         # hard window
-#         left = 0
+#         # hard window - Khi đề bài cho chỉ số window kết thúc là K
+#         n_windows_K = n - k + 1
+#         for i in range(n_windows_K):
+#             first_k = i
+#             last_k = i + k - 1
+#             window = nums[last] - nums[first]
+#             res = min(res, window)
+#         return res
+            
+# dynamic window - khi chưa biết đề bài cho chỉ số window cuối là bao nhiêu chỉ cho target duy nhất
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        if k == 1: return 0
+        nums.sort()
+        n = len(nums)
+        res = float('inf')
+        left = 0
+        for right in range(n):
+            if right - left + 1 == k:
+                res = min(res, nums[right] - nums[left])
+                left += 1
+        return res
 
         
