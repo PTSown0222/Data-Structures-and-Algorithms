667168339e040075cdde9064254eb378b3181792
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-15 16:07:30 +0700

    Time: 7 ms (68.46%), Space: 19.3 MB (60.86%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -4,13 +4,23 @@ class Solution:
         nums.sort()
         n = len(nums)
         res = float('inf')
-        left = 0
+        # hard window - Khi đề bài cho chỉ số window kết thúc là K
         n_windows_K = n - k + 1
-        #length_window = right - left + 1
-        #right = left + k - 1
-        for right in range(n_windows_K):
-            current_diff = nums[right + k - 1] - nums[right]
-            res = min(res, current_diff)
+        for i in range(n_windows_K):
+            first = i
+            last = i + k - 1
+            window = nums[last] - nums[first]
+            res = min(res, window)
         return res
             
+# dynamic window - khi chưa biết đề bài cho chỉ số window cuối là bao nhiêu chỉ cho target duy nhất
+# class Solution:
+#     def minimumDifference(self, nums: List[int], k: int) -> int:
+#         if k == 1: return 0
+#         nums.sort()
+#         n = len(nums)
+#         res = float('inf')
+#         # hard window
+#         left = 0
 
+        
