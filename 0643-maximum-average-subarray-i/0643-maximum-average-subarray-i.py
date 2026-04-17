239d5427a462830e55f3256c6fa224e6be04dffc
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-17 16:01:31 +0700

    Time: 50 ms (88.43%), Space: 29.2 MB (55.54%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,16 +3,30 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        left = 0
-        curr_sum = 0
-        ans = float('-inf')
-        for right in range(n):
-            curr_sum += nums[right]
+        
+        # dynamic sliding window
+        # left = 0
+        # curr_sum = 0
+        # ans = float('-inf')
+        # for right in range(n):
+        #     curr_sum += nums[right]
 
-            if right - left + 1 > k:
-                curr_sum -= nums[left]
-                left += 1
+        #     if right - left + 1 > k:
+        #         curr_sum -= nums[left]
+        #         left += 1
 
-            if right - left + 1 == k:
-                ans = max(ans, curr_sum)
-        return ans/k
+        #     if right - left + 1 == k:
+        #         ans = max(ans, curr_sum)
+        # return ans/k
+
+        # fixed sliding window
+
+        current_window_sum = sum(nums[:k])
+        max_sum = current_window_sum
+
+        for i in range(k, n):
+            current_window_sum = current_window_sum + nums[i] - nums[i - k]
+  
+            if current_window_sum > max_sum:
+                max_sum = current_window_sum
+        return max_sum/k
