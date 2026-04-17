c96994cd2ead78114460476fdc611f02636bfff4
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-17 13:34:46 +0700

    Time: 25 ms (63.07%), Space: 30.8 MB (89.36%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        # hash set + sliding window
+        n = len(s)
+        k = 10
+        seen = set()
+        repeated = set()
+        for i in range(n - k + 1):
+            sub_adn_seq = s[i:i+k]
+            if sub_adn_seq in seen:
+                repeated.add(sub_adn_seq)
+            else:
+                seen.add(sub_adn_seq)
+        return [i for i in repeated]
+
+        # hashmap + sliding window
