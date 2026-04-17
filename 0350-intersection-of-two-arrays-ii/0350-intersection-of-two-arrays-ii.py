0647f56581ed5618ff32c007419e813a97dc2632
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-17 09:14:06 +0700

    Time: 0 ms (100%), Space: 19.5 MB (28.6%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,20 +1,35 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
+        # nums1.sort()
+        # nums2.sort()
         n = len(nums1)
         m = len(nums2)
         intersect = []
-        i = 0
-        j = 0
-        while i < n and j < m:
-            if nums1[i] == nums2[j]:
-                intersect.append(nums1[i])
-                i+=1
-                j+=1
-            elif nums1[i] < nums2[j]:
-                i+=1
-            else:
-                j+=1
+        # i = 0
+        # j = 0
+        # while i < n and j < m:
+        #     if nums1[i] == nums2[j]:
+        #         intersect.append(nums1[i])
+        #         i+=1
+        #         j+=1
+        #     elif nums1[i] < nums2[j]:
+        #         i+=1
+        #     else:
+        #         j+=1
+        # return intersect
+
+        # way2 : hashmap
+        # idea: đếm tần suất nums1 tạo thành 1 lookup table
+        # iterate qua nums2 sau đó retrieve về nums1 xem có giống nhau không
+        # nums1: {2:2}
+        if n > m:
+            return self.intersect(nums2,nums1)
+        cnt = Counter(nums1)
+        
+        for i in nums2:
+            if cnt[i] > 0:# có xuất hiện ở cả 2 mảng
+                intersect.append(i)
+                cnt[i] -= 1
         return intersect
+
             
