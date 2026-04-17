57730b946f62c412cd87bf58517247a9210c38b0
Phương Thế Sơn <25210032@ms.uit.edu.vn>
2026-04-17 11:17:13 +0700

    Time: 30 ms (10.78%), Space: 19.8 MB (64.33%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+
+        n = len(s)
+        arr = list(s)
+        left = 0
+        pos = 0
+        for right in range(n + 1):
+            if right == n or arr[right] == " ":
+                start, end = left, right - 1
+                while start < end:
+                    arr[start], arr[end] = arr[end], arr[start]
+                    start += 1
+                    end -= 1
+                
+                left = right + 1
+        
+        return "".join(arr)
+
+
+                
